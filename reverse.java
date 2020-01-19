8386890a7198e2d3f93b720cb0817f8427eb1d93
Harmanjit Singh <57390891+harman20@users.noreply.github.com>
2020-01-19 19:51:58 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Reverse { 
+  
+    public static void main(String[] args) 
+    { 
+        Scanner s=new Scanner(System.in);
+        String s[] = "i like this program very much".split(" "); 
+        String str = ""; 
+        for (int i = s.length - 1; i >= 0; i--) { 
+            ans += s[i] + " "; 
+        } 
+        System.out.println("Reversed String:"); 
+        System.out.println(str.substring(0, str.length() - 1)); 
+    } 
+} 
